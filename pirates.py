18f39c292846024298b5d3226b721cc734863c69
Jeff Ripley <666v4nz@gmail.com>
2017-03-09 19:28:30 -0400

    Add fullscreen command line option

--- pirates.py
+++ pirates.py
@@ -2,19 +2,26 @@ __author__ = 'jripley'
 from states import *
 import config
 from RoguePy.Game import Game
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Pirates of Rogue Basin')
+parser.add_argument('--full', dest='fullScreen', action='store_true', help='run the game in fullscreen mode')
+
+args = parser.parse_args()
+print args
+fullscreen = args.fullScreen
 
 # Shenanigans to get RoguePy in the search path when it's the project root.
 import sys
 from config import *
 
 import RoguePy
-from RoguePy.Input import Keys
-from RoguePy.UI import Elements
 
 RoguePy.setFps(config.fps)
 
 
-game = Game("Pirates of Rogue Basin", config.layout['uiWidth'], config.layout['uiHeight'], False)
+game = Game("Pirates of Rogue Basin", config.layout['uiWidth'], config.layout['uiHeight'], fullscreen)
 game.addState(SplashState('splash'))
 game.addState(GenerateState('generate'))
 game.addState(PlayState('play'))
